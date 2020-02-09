6c46cf0c33ed8124d086cf8a16726614029ea105
harshit-saini <harshitr.x.t.10@gmail.com>
2020-02-09 13:33:11 +0530

    ejs working

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require("express");
 require("dotenv").config();
+const ejs = require("ejs");
+const expressEjsLayouts = require("express-ejs-layouts");
+
+const indexRouter = require("./routes/indexRouter");
+
 const app = express();
 
+// settings to serve static files
+app.use(express.static("public"))
 
 
+// setting for the view engine
+app.set("views", "view");
+app.set("view engine", "ejs");
+app.use(expressEjsLayouts);
+
+// 
+
+app.use("/", indexRouter);
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
