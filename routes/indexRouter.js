5eecc52b97117da09fd104f8a24c87dba43fbee1
harshit-saini <harshitr.x.t.10@gmail.com>
2020-02-09 15:23:50 +0530

    setup complete

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,6 +5,9 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
     res.render("index");
 })
+router.get("/login", (req, res, next) => {
+    res.render("login");
+})
 
 
 module.exports = router;
